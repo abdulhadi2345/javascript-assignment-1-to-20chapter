44cd15d4b92be3d2bd0cdc75773471c882540347
AbdulHadi <AH1920081908@gmail.com>
2025-06-16 11:09:18 +0500

    chapter 01
    javascript assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+// Exercise 1: Greet the user
+    alert("Hello Visitor! Welcome to our website.");
+
+    // Exercise 4: Multiple alerts in sequence
+    alert("Welcome to JavaScript!");
+    alert("Happy Learning!");
+    alert("Alerts help convey messages.");
+    
+    // Exercise 5: Message in developer console
+    console.log("This message is generated from the browser console.");
+
+    // Exercise 6: Additional alert for your project
+    alert("This is an alert used in your existing project!");
+
+   // Exercise 7a: Script in the HEA
+    // Script in the head - may run before page is fully loaded
+    console.log("Script running from HEAD section.");
+
+  // Exercise 7b: Script at the end of the body -->
+    console.log("Script running at the end of BODY section.");
+  
